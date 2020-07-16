0ed3c1b431a840764ab3634498eff947908603c0
Marcelo Ferreira <mah.tf@hotmail.com>
2020-07-16 05:21:08 -0300

    Add course module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { CourseModule } from './courses/course.module';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { CourseInfoComponent } from './courses/course-info.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './errors/error404.components';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    CourseInfoComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
       {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
-      {
         path: '**', component: Error404Component
       }
     ]),
-    HttpClientModule
+    HttpClientModule,
+    CourseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
